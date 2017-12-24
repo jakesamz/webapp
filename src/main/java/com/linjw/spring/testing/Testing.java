74ef72403f2364aab3f692d4d8974e68c6b8d2e9
jakesamz <linjw1024@gmail.com>
2017-12-24 22:48:37 +0800

    spring-test

---
+++ src/main/java/com/linjw/spring/testing/Testing.java
@@ -0,0 +1,12 @@
+package com.linjw.spring.testing;
+
+import org.springframework.test.context.ContextConfiguration;
+
+@ContextConfiguration
+public class Testing {
+	
+	public void a() {
+		
+	}
+
+}
