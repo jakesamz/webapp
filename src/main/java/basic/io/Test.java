3e968041b9b18b66c9e6be0b423dfcc5cd96d5b4
jakesamz <jake.jw.lin@pccw.com>
2018-01-23 19:13:44 +0800

    added io

---
+++ src/main/java/basic/io/Test.java
@@ -0,0 +1,33 @@
+package basic.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Test {
+
+	public static void main(String[] args) throws IOException {
+
+		String inputFileName = "data.txt";
+		//String outputFileName = "data-after.txt";
+		String currentFilePath = Test.class.getResource("").getPath();
+		
+		BufferedReader bufReader = new BufferedReader(
+				new FileReader(currentFilePath + inputFileName));
+
+		//BufferedWriter bw = new BufferedWriter(new FileWriter(currentFilePath + outputFileName));
+		
+		String tableName = "PRODUCT";
+		String valueString = null;
+		
+		while ((valueString = bufReader.readLine()) != null) {
+			String sub1 = valueString.substring(0, valueString.indexOf(","));
+			String sub2 = valueString.substring(valueString.indexOf("_") + 1, valueString.indexOf(","));
+			sub1 = String.format("%-30s", sub1);
+			System.out.println(sub1 + tableName + "." + sub2 + "%TYPE,");
+		}
+
+		bufReader.close();
+	}
+
+}
