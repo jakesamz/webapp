d3b23a185ffc3ccb4ebecf79adc76bff45435197
jakesamz <jake.jw.lin@pccw.com>
2019-08-23 18:09:42 +0800

    20190823-01

---
+++ src/main/java/basic/jvm/chapter2/ExceedEclipseHeapSize.java
@@ -0,0 +1,24 @@
+package basic.jvm.chapter2;
+
+/**
+ * 大对象直接进入老年代
+ * 1. 适合serial和ParNew收集器
+ * 2. 不适用Parallel Scavenge收集器
+ * 
+ * XX:PretenureSizeThreshold=3145728 -XX:+UseSerialGC
+ * or
+ * XX:PretenureSizeThreshold=3145728 -XX:+UseParNewGC
+ * @author Administrator
+ *
+ */
+public class ExceedEclipseHeapSize {
+	
+	private static final int _1MB = 1024*1024;
+	
+	public static void main(String[] args) {
+		
+		byte[] allocation1 = new byte[500 * _1MB];
+		
+	}
+
+}
